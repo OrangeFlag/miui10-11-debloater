0adb3ed6492960384308b280e99408db09c7b70e
Ilya Selivanau <25643404+OrangeFlag@users.noreply.github.com>
2022-08-14 16:02:03 +0300

    Update miui-debloat.cmd

--- miui-debloat.cmd
+++ miui-debloat.cmd
@@ -1,4 +1,5 @@
 adb devices
+
 adb shell pm uninstall -k --user 0 com.miui.notes
 adb shell pm uninstall -k --user 0 com.miui.analytics
 adb shell pm uninstall -k --user 0 com.google.android.videos
@@ -56,3 +57,47 @@ adb shell pm uninstall -k --user 0 com.google.android.projection.gearhead
 adb shell pm uninstall -k --user 0 com.xiaomi.mi_connect_service
 adb shell pm uninstall -k --user 0 com.xiaomi.miplay_client
 adb shell pm uninstall -k --user 0 com.miui.miservice
+adb shell pm uninstall –k ––user 0 com.facebook.appmanager
+adb shell pm uninstall –k ––user 0 com.facebook.services
+adb shell pm uninstall –k ––user 0 com.facebook.system
+adb shell pm uninstall –k ––user 0 com.mintgames.triplecrush.tile.fun
+
+adb uninstall com.igg.android.lordsmobile
+adb uninstall video.like
+adb uninstall com.booking
+adb uninstall com.opera.browser
+adb uninstall com.yandex.zenkitpartnerconfig 
+adb uninstall ru.yandex.searchplugin 
+adb uninstall com.yandex.zen
+adb uninstall com.netflix.mediaclient
+adb uninstall com.mi.globalbrowser
+adb shell pm uninstall –k ––user 0 com.mi.globalbrowser
+adb shell pm uninstall –k ––user 0 com.miui.wmsvc
+adb uninstall com.miui.wmsvc
+adb shell pm uninstall –k ––user 0 com.miui.phrase
+adb uninstall com.miui.phrase
+adb uninstall com.block.puzzle.game.hippo.mi
+adb shell pm uninstall –k ––user 0 com.block.puzzle.game.hippo.mi
+adb shell pm uninstall –k ––user 0 com.sukhavati.gotoplaying.bubble.BubbleShooter.mint
+adb uninstall com.sukhavati.gotoplaying.bubble.BubbleShooter.mint
+adb shell pm uninstall –k ––user 0 sg.bigo.live
+adb uninstall sg.bigo.live
+adb uninstall com.micredit.in
+adb uninstall com.facemoji.lite.xiaomi
+adb shell pm uninstall –k ––user 0 com.facemoji.lite.xiaomi
+adb shell pm uninstall –k ––user 0 com.ebay.carrier
+adb uninstall com.ebay.carrier
+adb uninstall com.mi.global.pocobbs
+adb uninstall com.google.android.apps.youtube.music
+adb uninstall com.miui.cleaner
+adb shell pm uninstall –k ––user 0 com.miui.cleaner
+adb shell pm uninstall –k ––user 0 com.mi.globalbrowser
+adb uninstall com.mi.globalbrowser
+adb uninstall com.xiaomi.mipicks
+adb shell pm uninstall –k ––user 0 com.xiaomi.mipicks
+adb uninstall com.xiaomi.calendar
+adb shell pm uninstall –k ––user 0 com.xiaomi.calendar
+adb uninstall com.nll.asr
+
+
+
